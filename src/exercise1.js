1fa7405ff0fda74f7b0003b3b57fb63d8502d7f7
FloFloo17 <flo.erwan@gmail.com>
2017-04-21 10:46:51 +0200

    passe le test n°1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,10 +1,30 @@
 const chalk = require('chalk');
+var _ = require('lodash');
+
 
 console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 
 const zip = (input) => {
 
-  // Your future job begins here ...
+var output = [];
+
+
+// _.forEach(input, function(colection) {
+// 	_.forEach(colection, function(item, key) {
+// 		// output.
+
+// 		// var colectionOutput = [];
+
+// 		// colectionOutput[key] = item;
+
+// 		// output.push(colectionOutput);
+// 	});
+// });
+
+// output.push(colectionOutput);
+
+// console.log(output);
+return output;
 }
 
 module.exports = {
