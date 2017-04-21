e82a23bb87c995c2915faf3af2754a8ffd6bd8d9
FloFloo17 <flo.erwan@gmail.com>
2017-04-21 10:50:51 +0200

    au mois ça passe
    :'(

--- src/exercise3.js
+++ src/exercise3.js
@@ -4,7 +4,7 @@ console.log(chalk.cyan('\nEXERCISE 3 LOADED\n'));
 
 const reverseString = (input) => {
 
-  // Your future job begins here ...
+  return input.split('').reverse().join('');
 }
 
 module.exports = {
